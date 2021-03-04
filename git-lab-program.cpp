92970e3fead7c164e3a5bdd37a17207c9b303e43
Colin Schuster <cs848519@ohio.edu>
2021-03-04 14:53:09 -0500

    git-lab-program

---
+++ git-lab-program.cpp
@@ -0,0 +1,18 @@
+/**
+ *   @file: git-lab-program.cpp
+ * @author: Colin Schuster
+ *   @date: 3/4/2021
+ *  @brief: git-lab-program.cpp
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+    
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
